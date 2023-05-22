0ae2c0953a7350a5b3bb408b4edfc09af7ec7a86
Jesse <73019779+YamaYAML@users.noreply.github.com>
2023-05-22 15:51:01 +0200

    Update BossMechanicHelper.lua

--- BossMechanicHelper.lua
+++ BossMechanicHelper.lua
@@ -2,14 +2,12 @@ SLASH_BMH1 = "/bmh"
 
 SlashCmdList["BMH"] = function(msg)
     if msg == "config" then
-        -- Show the configuration frame
         if BossMechanicHelperConfigFrame then
             BossMechanicHelperConfigFrame:Show()
         else
             message("Configuration frame is not loaded. Please check if all the addon files are properly installed.")
         end
     else
-        -- Show the dropdown menu
         if BossMechanicHelperDropdown then
             ToggleDropDownMenu(1, nil, BossMechanicHelperDropdown, "cursor", 0, 0)
         else
@@ -41,17 +39,7 @@ function BossMechanicHelper_InitializeDropdown(self, level)
 end
 
 function BossMechanicHelper_ConfigSave()
-    -- Save the changes made in the configuration frame
     if BossMechanicHelperConfigScrollFrame.buttons then
         for i=1, table.getn(BossMechanicHelperConfigScrollFrame.buttons) do
             local button = BossMechanicHelperConfigScrollFrame.buttons[i]
-            if button and BossMechanicHelperDefaultMechanics[button.instance] and BossMechanicHelperDefaultMechanics[button.instance][button.boss] then
-                BossMechanicHelperDefaultMechanics[button.instance][button.boss] = button.editBox:GetText()
-            else
-                message("Failed to save the mechanic. Please make sure the mechanic is associated with a valid boss and instance.")
-            end
-        end
-    else
-        message("No configuration buttons found. Please check if the configuration frame is properly loaded.")
-    end
-end
+            if button and BossMechanicHelperDefaultMechanics[button.instance] and BossMechanicHelperDefaultMechanics[button.instance
