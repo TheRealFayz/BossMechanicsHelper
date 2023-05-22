4fed6ff1dfaf070a957c933646c15fada34919b2
Jesse <73019779+YamaYAML@users.noreply.github.com>
2023-05-22 16:04:30 +0200

    Create BossMechanicHelperDefaultMechanics.lua

---
+++ BossMechanicHelperDefaultMechanics.lua
@@ -0,0 +1,44 @@
+BossMechanicHelperDefaultMechanics = {
+    ["Molten Core"] = {
+        ["Lucifron"] = "Dispel Curse. Remove Impending Doom (8k dmg in 10s) and Lucifron's Curse (-5% chance to hit, +100% cost to abilities).",
+        ["Magmadar"] = "Tank in the center. Run away from Frenzy (AoE fire damage). Tranquilizing Shot to remove frenzy.",
+        ["Gehennas"] = "Dispel Gehennas's Curse (-75% healing). Tank adds away from Gehennas.",
+        ["Garr"] = "Warlock tank adds. Do not banish. Spread out to avoid chaining Harvest.",
+        ["Baron Geddon"] = "Avoid Living Bomb (run away from raid). Dispel Ignite Mana.",
+        ["Shazzrah"] = "Dispel Curse of Shazzrah. Tank teleports and wipes aggro, offtank taunts.",
+        ["Sulfuron Harbinger"] = "Tank and kill adds. Dispel Inspire (healing received increase).",
+        ["Golemagg the Incinerator"] = "Tank boss and adds separately. Adds become invulnerable at 10%.",
+        ["Majordomo Executus"] = "Kill healers first. Tank and banish adds. ",
+        ["Ragnaros"] = "Melee out on Wrath of Ragnaros. Tank knockback, offtank taunts."
+    },
+    ["Blackwing Lair"] = {
+        ["Razorgore the Untamed"] = "Destroy eggs with Orb of Domination, protect the controller from mobs. Phase 2: kill the boss, avoid conflagration.",
+        ["Vaelastrasz the Corrupt"] = "High DPS race. Healing reduced by 50%, mana burns quickly. Tanks swap due to Burning Adrenaline.",
+        ["Broodlord Lashlayer"] = "Remove Blast Wave. Avoid Cleave and Mortal Strike.",
+        ["Firemaw"] = "Hide behind pillar during Wing Buffet. Tanks swap due to stacks of Flame Buffet.",
+        ["Ebonroc"] = "Tanks swap due to Shadow of Ebonroc. Don't heal the tank with the debuff.",
+        ["Flamegor"] = "Tanks swap due to stacks of Flame Buffet. Tranquilizing Shot during Frenzy.",
+        ["Chromaggus"] = "Dispel debuffs. Match resistance to breath. Tranquilizing Shot during Enrage.",
+        ["Nefarian"] = "Phase 1: kill adds. Phase 2: Class call handling. Priest - no direct heals. Warlock - kill Infernals."
+    },
+    ["Onyxia's Lair"] = {
+        ["Onyxia"] = "Phase 1: Tank and spank. Phase 2: Avoid Fireball, kill Whelps. Phase 3: Avoid Bellowing Roar (fear)."
+    },
+    ["Zul'Gurub"] = {
+        ["High Priest Venoxis"] = "Move out of poison clouds. Avoid Holy Fire. Dispel Renew.",
+        ["High Priestess Jeklik"] = "Kill bats. Interrupt heals. Dispel Mind Flay.",
+        ["High Priestess Mar'li"] = "Kill Spawn of Mar'li. Interrupt drain life. Dispel poison.",
+        ["High Priest Thekal"] = "Kill all tigers simultaneously. Tank away from Zealots.",
+        ["High Priestess Arlokk"] = "Kill panthers. Tank boss away from raid.",
+        ["Hakkar"] = "Tank in center. Mind Control priests for Greater Heal. Drain Power to reduce damage."
+    },
+    ["Ruins of Ahn'Qiraj"] = {
+        ["Kurinnaxx"] = "Avoid Sand Trap. Tank swap at 10 stacks of Mortal Wound.",
+        ["General Rajaxx"] = "Kill waves of adds. Tank swap at 3 stacks of Mortal Strike.",
+        ["Moam"] = "Kill Mana Fiends. Tank away from raid due to Arcane Explosion.",
+        ["Buru the Gorger"] = "Kite boss over eggs. Kill eggs to damage boss.",
+        ["Ayamiss the Hunter"] = "Avoid poison clouds. Tank swap at 10 stacks of Stinger Spray.",
+        ["Ossirian the Unscarred"] = "Kite boss to crystals to remove boss buff."
+    },
+    -- more instances and bosses
+}
